b4f7188b1fed0e38e502941fc3b40cc6f09b9421
Jezmael Basilio <jezmaelbasilio@gmail.com>
2021-05-14 09:35:23 -0300

    Comentando os codigos

--- lista-tarefas/app.js
+++ lista-tarefas/app.js
@@ -1,29 +1,45 @@
+// 1. Inicializamos primeiro o array vazio.
+// Ele que irá receber as tarefas adicionadas
 const lista = [];
 
-// 1. Chamar uma função quando clicar no botão
+// 2. Função que vai ser chamada pelo click do botão
 function adicionar() {
-  // 2. Pegar o texto e colocar em um vetor
-  const texto = document.getElementById("tarefa");
+  // 3. Aqui pegamos o elemento Input que tem o nome da tarefa
+  const input = document.getElementById("tarefa");
+  // input.value pega apenas o texto que foi digitado no input
+  const tarefa = input.value;
 
-  const jaExiste = lista.indexOf(texto.value);
+  // Aqui verificamos se já existe uma tarefa com o mesmo nome
+  // caso tenha, o indexOf vai retornar o índice do texto no vetor
+  const jaExiste = lista.indexOf(tarefa);
 
-  if (texto.value !== "" && jaExiste === -1) {
-    lista.push(texto.value);
+  // 4. Verifica se a tarefa não é um texto vazio e se não foi encontrato
+  // o texto dentro do vetor (por isso o -1)
+  if (tarefa !== "" && jaExiste === -1) {
+    // 5. adicionamos a tarefa dentro do vetor
+    lista.push(tarefa);
 
-    texto.value = "";
+    // apagamos o texto que o usuário escreveu no input
+    input.value = "";
 
+    // chamamos a função reponsável por atualizar a lista no HTML
     criaLista();
   }
 }
 
-// 3. Exibir a lista atualizada
+// 6. Exibir a lista atualizada
 function criaLista() {
+  // declaramos uma variável com o texto vazio
   let html = "";
 
+  // depois adicionamos as tags HTML <li> com o nome da tarefa
   for (let tarefa of lista) {
     html = html + `<li>${tarefa}</li>`;
   }
 
+  // aqui pegamos o elemento ul do HTML
   const ul = document.getElementById("lista");
+
+  // adicionamos as tags HTML dos <li> lá dentro da ul
   ul.innerHTML = html;
 }
